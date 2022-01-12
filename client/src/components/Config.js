7616fcd710155ade5e96e4cfd7fb9209dddc7a2e
flowersayo <flowersayo0811@ewhain.net>
2022-01-13 03:45:40 +0900

    <feat> Top 20 영화 불러오기
    메인이미지 생성, 영화 이미지를 보여주는 그리드카드 생성

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -3,6 +3,6 @@ export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
 
-export const IMAGE_BASE_URL='http://image.tmdb.org/t/p/';
+export const IMAGE_BASE_URL='https://image.tmdb.org/t/p/';
 
 export const API_KEY ='e5c576d261c50f56ce31fb028963759d';
---
+++ client/src/components/views/commons/GridCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Col} from 'antd';
+
+function GridCard(props) {
+    return (
+        <Col lg={6} md={8} xs={24}/* 한 줄이 24 size */> 
+        <div style={{ position : 'relative'}}>
+           <a href ={`/movie/${props.movieId}`/*영화 상세페이지로 이동*/}>
+               <img style={{ width : '100%', height: '400px' }}src={props.image} alt={props.movieName}/>
+           </a>
+
+        </div>
+        </Col>
+    )
+}
+
+export default GridCard
