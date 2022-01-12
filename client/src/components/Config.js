d0cc408032806d24674a82ae365e1a182dda3213
flowersayo <flowersayo0811@ewhain.net>
2022-01-12 23:15:59 +0900

    <layout> LandingPage 틀

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL='http://image.tmdb.org/t/p/';
+
+export const API_KEY ='e5c576d261c50f56ce31fb028963759d';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FaCode } from "react-icons/fa";
+import {API_URL,API_KEY} from '../../Config';
 
 function LandingPage() {
+    useEffect(()=>{
+      const endpoint=`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US%page=1`;
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(result =>console.log(result));
+    },[])
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+       <div style={{ width : '100%', margin: '0'}}>
+           {/* main image */}
+
+           <div style={{ width : '85%', margin: '1rem auto'}}>
+            <h2>Movies by latest</h2>
+            <hr/>
+
+            {/*Movie Grid Card */}
+
+           </div>
+            <div style={{display :'flex', justifyContent: 'center'}}>
+                <button>Load more</button> 
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+       </div>
     )
 }
 
-export default LandingPage
+export default LandingPage;
+
